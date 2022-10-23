27231d42e5515093e6e56fb44b05bf16ce6b7b16
hazyparker <2559272883@qq.com>
2022-10-24 00:09:48 +0800

    update to 3.5

--- book_examples/Chapter3-MDP1.py
+++ book_examples/Chapter3-MDP1.py
@@ -52,3 +52,70 @@ def compute(state_mat, reward_func, discount):
 
 V = compute(P, rewards, gamma)
 print("each state value of MRP is: \n", V)
+
+S = ["s1", "s2", "s3", "s4", "s5"]  # state group
+# action group
+A = ["hold s1", "go s1", "go s2", "go s3", "go s4", "go s5", "probably to go"]
+
+# state transition matrix (function)
+# different dut to actions
+P = {
+    "s1-hold s1-s1":  1.0,
+    "s1-goto s2-s2":  1.0,
+    "s2-goto s1-s1":  1.0,
+    "s2-goto s3-s3":  1.0,
+    "s3-goto s4-s4":  1.0,
+    "s3-goto s5-s5":  1.0,
+    "s4-goto s5-s5":  1.0,
+    "s4-probably goto-s2": 0.2,
+    "s4-probably goto-s3": 0.4,
+    "s4-probably goto-s4": 0.4
+}
+
+# reward function
+R = {
+    "s1-hold s1": -1,
+    "s1-goto s2": 0,
+    "s2-goto s1": -1,
+    "s2-goto s3": -2,
+    "s3-goto s4": -2,
+    "s3-goto s5": 0,
+    "s4-goto s5": 10,
+    "s4-probably goto": 1,
+}
+
+MDP_sample = (S, A, P, R, gamma)
+
+# strategy 1
+Pi_1 = {
+    "s1-hold s1": 0.5,
+    "s1-goto s2": 0.5,
+    "s2-goto s1": 0.5,
+    "s2-goto s3": 0.5,
+    "s3-goto s4": 0.5,
+    "s3-goto s5": 0.5,
+    "s4-goto s5": 0.5,
+    "s4-probably goto": 0.5,
+}
+
+# strategy 2
+Pi_2 = {
+    "s1-hold s1": 0.6,
+    "s1-goto s2": 0.4,
+    "s2-goto s1": 0.3,
+    "s2-goto s3": 0.7,
+    "s3-goto s4": 0.5,
+    "s3-goto s5": 0.5,
+    "s4-goto s5": 0.1,
+    "s4-probably goto": 0.9,
+}
+
+
+def join(str1, str2):
+    return str1 + '-' + str2
+
+
+def sample(mdp, strategy, timestep_max, number):
+    state, action, state_mat, reward, discount = mdp
+
+
